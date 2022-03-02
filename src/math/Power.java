5c44e9c17d7cba76a106580c31b9f09bc3e878ca
Fatih Karatay <fatih.karatay@ngc.com>
2022-03-01 22:02:54 -0500

    power large and negative numbers is added.

--- src/math/Power.java
+++ src/math/Power.java
@@ -42,6 +42,20 @@ public class Power {
     }
 
     public static double power(int x, int n) {
-        return 1.0;
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1;
+        double currentProduct = x;
+
+        for (long i = N; i > 0; i = i/2) {
+            if (i % 2 == 1)
+                ans = currentProduct * ans;
+            currentProduct = currentProduct * currentProduct;
+        }
+        return ans;
     }
 }
