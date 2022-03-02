fa768da31c932abc390d2b98fa2616d74ddd5188
Fatih Karatay <fatih.karatay@ngc.com>
2022-03-01 21:58:55 -0500

    power recursive approach is added

---
+++ src/math/Power.java
@@ -0,0 +1,38 @@
+package math;
+
+/**
+ * computing the power of any numbers.
+ * We have both recursive and iterative way.
+ * Also, we have bit manipulation approach for the really large and negative numbers.
+ */
+
+public class Power {
+
+    public static void main(String[] args) {
+        int a = 5, b = 4;
+
+        System.out.println(powerRec(a, b));
+        System.out.println(powerItr(a, b));
+        System.out.println(power(a, b));
+    }
+
+    public static int powerRec(int a, int b) {
+        // base case
+        if (b == 0)
+            return 1;
+        int temp = powerRec(a, b / 2);
+        int result = temp * temp;
+        if (b % 2 == 1)
+            result *= a;
+
+        return result;
+    }
+
+    public static int powerItr(int a, int b) {
+        return 1;
+    }
+
+    public static double power(int x, int n) {
+        return 1.0;
+    }
+}
